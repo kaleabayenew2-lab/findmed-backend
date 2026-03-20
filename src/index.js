a838bb3860618fde7df66a1051a888e7378fcdaa
kaleab <your_email@example.com>
2026-03-20 16:28:52 +0300

    Fix CORS and add origin for flutter web 127.0.0.1:3001

--- src/index.js
+++ src/index.js
@@ -22,7 +22,11 @@ const allowedOrigins = [
   'http://localhost:3000',
   'https://localhost:3000',
   'http://127.0.0.1:3000',
-  'https://127.0.0.1:3000'
+  'https://127.0.0.1:3000',
+  'http://localhost:3001',
+  'https://localhost:3001',
+  'http://127.0.0.1:3001',
+  'https://127.0.0.1:3001'
 ];
 
 // If FRONTEND_ORIGIN is set, add it to the allowed origins (if not already present)
